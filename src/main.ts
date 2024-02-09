adae61bee190791802967356881f3499ac0a316c
Alex Barakhov <abarakhov@gmail.com>
2024-02-09 22:16:01 +0100

    formated main.ts file

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,8 @@ let count = factory(0, 1)
 
 const start_at_control = document.getElementById('start_at') as HTMLInputElement
 const step_control = document.getElementById('step') as HTMLInputElement
-const count_button = document.querySelector(
-  '.count_button'
-) as HTMLButtonElement
-const current_count = document.querySelector(
-  '.current_count'
-) as HTMLSpanElement
+const count_button = document.querySelector('.count_button') as HTMLButtonElement
+const current_count = document.querySelector('.current_count') as HTMLSpanElement
 
 function update_count_and_reset_counter() {
   const start_at_number = parseInt(start_at_control.value, 10)
