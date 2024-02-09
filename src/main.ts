f404217dbc27cb3c04fb831a4b6d54fd9412fcf5
Alex Barakhov <abarakhov@gmail.com>
2024-02-09 21:16:15 +0100

    added logic

--- src/main.ts
+++ src/main.ts
@@ -2,26 +2,23 @@ import { factory } from "./factory";
 
 let count = factory(0, 1);
 
-function update_count_and_reset_counter() {}
-
-const start_at_control = document.getElementById(
-  "start_at",
-) as HTMLInputElement;
-
+const start_at_control = document.getElementById("start_at") as HTMLInputElement;
 const step_control = document.getElementById("step") as HTMLInputElement;
+const count_button = document.querySelector(".count_button") as HTMLButtonElement;
+const current_count = document.querySelector(".current_count") as HTMLSpanElement;
 
-start_at_control?.addEventListener("change", () => {});
+function update_count_and_reset_counter() {
+  let start_at_number = parseInt(start_at_control.value, 10)
+  let step_number = parseInt(step_control.value, 10)
 
-step_control?.addEventListener("change", () => {});
+  count = factory(start_at_number, step_number)
+  update_count();
+}
 
-const count_button = document.querySelector(
-  ".count_button",
-) as HTMLButtonElement;
+function update_count() {
+  current_count.innerText = count().toString()
+}
 
-const current_count = document.querySelector(
-  ".current_count",
-) as HTMLSpanElement;
-
-function update_count() {}
-
+start_at_control?.addEventListener("change", update_count_and_reset_counter);
+step_control?.addEventListener("change", update_count_and_reset_counter);
 count_button.addEventListener("click", update_count);
