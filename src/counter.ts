bdfc03da2d345fa9d9882062a09030a32ea23d81
Alex Barakhov <abarakhov@gmail.com>
2024-02-13 01:29:04 +0100

    formatted files according to prettier

--- src/counter.ts
+++ src/counter.ts
@@ -1,26 +1,26 @@
-import { factory } from './factory';
+import { factory } from './factory'
 
-export let countValue = factory(0, 1);
+export let countValue = factory(0, 1)
 
 export function updateCountAndResetCounter(
   startInput: HTMLInputElement,
   stepInput: HTMLInputElement,
   currentCountDisplay: HTMLSpanElement
 ) {
-  countValue = factory(+startInput.value, +stepInput.value);
-  resetCounterDisplay(+startInput.value, currentCountDisplay);
+  countValue = factory(+startInput.value, +stepInput.value)
+  resetCounterDisplay(+startInput.value, currentCountDisplay)
 }
 
 export function updateCount(
   countFunction: () => number,
   currentCountDisplay: HTMLSpanElement
 ) {
-  currentCountDisplay.innerText = countFunction().toString();
+  currentCountDisplay.innerText = countFunction().toString()
 }
 
 function resetCounterDisplay(
   startValue: number,
   currentCountDisplay: HTMLSpanElement
 ) {
-  currentCountDisplay.innerText = startValue.toString();
+  currentCountDisplay.innerText = startValue.toString()
 }
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
-import { countValue, updateCountAndResetCounter, updateCount } from "./counter";
+import { countValue, updateCountAndResetCounter, updateCount } from './counter'
 
-const startInput = document.getElementById('start_at') as HTMLInputElement;
-const stepInput = document.getElementById('step') as HTMLInputElement;
-const countButton = document.querySelector('.count_button') as HTMLButtonElement;
-const currentCountDisplay = document.querySelector('.current_count') as HTMLSpanElement;
+const startInput = document.getElementById('start_at') as HTMLInputElement
+const stepInput = document.getElementById('step') as HTMLInputElement
+const countButton = document.querySelector('.count_button') as HTMLButtonElement
+const currentCountDisplay = document.querySelector(
+  '.current_count'
+) as HTMLSpanElement
 
-startInput?.addEventListener('change', () => updateCountAndResetCounter(startInput, stepInput, currentCountDisplay));
-stepInput?.addEventListener('change', () => updateCountAndResetCounter(startInput, stepInput, currentCountDisplay));
-countButton?.addEventListener('click', () => updateCount(countValue, currentCountDisplay));
+startInput?.addEventListener('change', () =>
+  updateCountAndResetCounter(startInput, stepInput, currentCountDisplay)
+)
+stepInput?.addEventListener('change', () =>
+  updateCountAndResetCounter(startInput, stepInput, currentCountDisplay)
+)
+countButton?.addEventListener('click', () =>
+  updateCount(countValue, currentCountDisplay)
+)
